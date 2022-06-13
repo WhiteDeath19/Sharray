e24486d3d23f5aeacf3a6a3c7f0fbb520902a53c
WhiteDeath19 <107235587+WhiteDeath19@users.noreply.github.com>
2022-06-13 16:30:28 +0530

    Update nwchk.go

--- nwchk.go
+++ nwchk.go
@@ -1,6 +1,17 @@
 package main 
 import "fmt"
 
+var x,y int32
+func add(){
+	fmt.Println("Enter first number")
+	fmt.Scanln(x)
+	fmt.Println("Enter second number")
+	fmt.Scanln(y)
+	z:= x+y
+	fmt.Println("The sum is",z)
+	return z
+
 func main(){
 	fmt.Println("Hello world????")
+	add()
 }
