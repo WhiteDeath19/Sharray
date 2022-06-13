14a3e3e4c7e7449b7271930a5ed5d458bdc524a2
WhiteDeath <shivanshu.tyagi@jukshio.com>
2022-06-13 12:52:02 +0530

    Added some features>>git status

--- helloworld.go
+++ helloworld.go
@@ -5,4 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Tried to change something!!!")
+	fmt.Println("Still learning the code !!!")
+
+	func add(x,y){
+		z:= x+y
+		fmt.Println("The Sum of numbers is",z)
+
+	}
 }
